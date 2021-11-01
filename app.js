5255ce2aa98ea35fc9f66f17c64ee1f383da5c63
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-11-01 16:22:22 +0530

    Add Initial db connection

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+// import packages
+require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 
+// create app express instance
 const app = express();
 
+// import db connection function
+const connectDB = require('./db/connect');
+// import middleware
+
+// import routes
+
+// use middleware
 const port = process.env.PORT || 5000;
 
 const start = async () => {
+  await connectDB(process.env.MONGO_URL);
   app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
   });
