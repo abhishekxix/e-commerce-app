a04e991a63127c434c896833ad8ddeae9aa017b7
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-11-01 16:07:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  app.listen(port, () => {
+    console.log(`App is running on port: ${port}`);
+  });
+};
+
+start();
