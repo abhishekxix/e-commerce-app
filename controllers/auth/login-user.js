89576aee2f6e472716fba61449047504aab2cac7
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-11-02 21:15:53 +0530

    Add login functionality

--- controllers/auth/login-user.js
+++ controllers/auth/login-user.js
@@ -1,5 +1,39 @@
+const { StatusCodes } = require('http-status-codes');
+const {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} = require('../../errors');
+const User = require('../../models/User');
+const { attachTokenCookie, createTokenUser } = require('../../utils');
+
 const logInUser = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    throw BadRequestError('Provide both email and password');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw NotFoundError(`No user found with email: ${email}`);
+  }
+
+  if (!user.isVerified) {
+    throw UnauthorizedError('Please verify your email id before you log in.');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw UnauthorizedError('Wrong password.');
+  }
+
+  const tokenUser = createTokenUser(user);
+
+  attachTokenCookie(res, tokenUser);
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 module.exports = logInUser;
