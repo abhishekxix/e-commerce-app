b24a67af5aec23a1ba803b85401745f61329b40b
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-11-03 10:56:13 +0530

    Add logout functionality.

--- controllers/auth/logout-user.js
+++ controllers/auth/logout-user.js
@@ -1,5 +1,13 @@
+const { StatusCodes } = require('http-status-codes');
+
 const logOutUser = async (req, res) => {
-  res.send('logout user');
+  res.cookie('token', '', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+  });
+  res.status(StatusCodes.OK).json({ msg: 'logged out' });
 };
 
 module.exports = logOutUser;
